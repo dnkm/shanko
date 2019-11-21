fd467dfd0b52ebaf0806490342b8d9a5b31ac224
David Chang <52216372+cinnagatari@users.noreply.github.com>
2019-11-21 14:48:09 -0800

    Changes from user to ID

--- src/libraries/user.js
+++ src/libraries/user.js
@@ -1,111 +1,120 @@
-let userbase = {
+let users = {
   david: {
-    username: "david",
-    password: "123",
+    id: "david",
+    password: "0000",
     nickname: "coffee",
     gender: 0,
-    cash: 999999,
+    cash: 500000,
     imgnumber: 0,
     win: 50,
     lose: 40
   },
   jay: {
-    username: "jay",
-    password: "123",
+    id: "jay",
+    password: "0000",
     nickname: "satisfiedlemon",
     gender: 0,
-    cash: 999999,
+    cash: 250000,
     imgnumber: 3,
     win: 50,
     lose: 40
   },
   daniel: {
-    username: "daniel",
-    password: "123",
+    id: "daniel",
+    password: "0000",
     nickname: "dnkm",
     gender: 0,
-    cash: 999999,
+    cash: 100000,
     imgnumber: 1,
     win: 50,
     lose: 40
   },
   matthew: {
-    username: "matthew",
-    password: "123",
+    id: "matthew",
+    password: "0000",
     nickname: "akai",
     gender: 0,
-    cash: 999999,
+    cash: 50000,
     imgnumber: 2,
     win: 50,
     lose: 40
   },
+  test00: {
+    id: "test00",
+    password: "0000",
+    nickname: "test",
+    gender: 0,
+    cash: 10000,
+    imgnumber: 4,
+    win: 0,
+    lose: 0
+  },
   guest1: {
-    username: "guest1",
-    password: "123",
+    id: "guest1",
+    password: "0000",
     nickname: "beep",
     gender: 0,
-    cash: 9999999,
+    cash: 450000,
     imgnumber: 4,
     win: 999,
     lose: 0
   },
   guest2: {
-    username: "guest2",
-    password: "123",
+    id: "guest2",
+    password: "0000",
     nickname: "boop",
     gender: 0,
-    cash: 999,
+    cash: 500,
     imgnumber: 5,
     win: 0,
     lose: 999
   }
 };
 
-let users = {};
+let ids = {};
+let sockets = {};
 
-function profile(user, socket) {
+function profile(socket) {
   socket.emit("resp_userinfo", {
-    nickname: userbase[user].nickname,
-    gender: userbase[user].gender,
-    cash: userbase[user].cash,
-    imgnumber: userbase[user].imgnumber,
-    win: userbase[user].win,
-    lose: userbase[user].lose
+    nickname: users[sockets[socket.id]].nickname,
+    gender: users[sockets[socket.id]].gender,
+    cash: users[sockets[socket.id]].cash,
+    imgnumber: users[sockets[socket.id]].imgnumber,
+    win: users[sockets[socket.id]].win,
+    lose: users[sockets[socket.id]].lose
   });
 }
 
 function login(data, socket, games) {
   if (
-    userbase[data.user] === undefined ||
-    userbase[data.user].password !== data.password
+    users[data.id] === undefined ||
+    users[data.id].password !== data.password
   ) {
     socket.emit("resp_login", { retcode: 1 });
     return;
   }
-  socket.join("users");
-  if (users[data.user] !== undefined) {
+  socket.join("ids");
+  if (ids[data.id] !== undefined) {
     updateSocket(data.user, socket, socket.id, games);
-    users[data.user] = { ...users[data.user], sid: socket.id };
-  } else users[data.user] = { sid: socket.id, rooms: [] };
+    ids[data.id] = { ...ids[data.id], sid: socket.id };
+  } else {
+    ids[data.id] = { sid: socket.id, room: "none" };
+    sockets[socket.id] = data.id;
+  }
   socket.emit("resp_login", { sid: socket.id, retcode: 0 });
 }
 
-function updateSocket(user, socket, sid, games) {
-  console.log(games);
-  users[user].rooms.forEach(room => {
-    games[room].slots.forEach(slot => {
-      if (slot.user === user) {
-        slot.sid = sid;
-      }
-    });
-    socket.join(room);
-  });
-  users[user].sid = sid;
+function updateSocket(id, socket, sid, games) {
+  if (ids[id].room !== "none") {
+    socket.join(ids[id].room);
+  }
+  delete sockets[ids[id].sid];
+  sockets[sid] = id;
+  ids[id].sid = sid;
 }
 
 module.exports = {
-  userbase: userbase,
-  users: users,
+  ids: ids,
   profile: profile,
   login: login
 };
