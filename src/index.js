89e471d70211b47c8bd7e51b6409477a1ec93fcd
David Chang <52216372+cinnagatari@users.noreply.github.com>
2019-11-30 19:07:25 -0800

    New Sockets
    - changeGender
    - changeImgNumber

--- src/index.js
+++ src/index.js
@@ -8,15 +8,17 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 io.on("connection", socket => {
   console.log("user " + socket.id + " has connected");
-  
+
   socket.use((socket, next) => {
     try {
-        if (socket.length > 1 && typeof socket[1] === 'string')
-            socket[1] = JSON.parse(socket[1])
-    } catch(err) {console.error(err); }
+      if (socket.length > 1 && typeof socket[1] === "string")
+        socket[1] = JSON.parse(socket[1]);
+    } catch (err) {
+      console.error(err);
+    }
     next();
-  })
-  
+  });
+
   socket.on("disconnect", () => console.log("user has disconnected"));
 
   // login sockets
@@ -26,6 +28,8 @@ io.on("connection", socket => {
   // main lobby sockets
   socket.on("rqst_rooms", () => socket.emit("resp_rooms", game.rooms));
   socket.on("rqst_join", data => game.join(data, socket, io));
+  socket.on("rqst_changegender", data => user.changeGender(data, socket));
+  socket.on("rqst_changeimgnumber", data => user.changeImgNumber(data, socket));
 
   // game sockets
 });
